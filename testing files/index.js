b7e4733a540d1d145574ff54c932a92bfba98ed8
Gavin Loughridge <gavinlough@gmail.com>
2017-11-10 11:58:02 -0600

    created testing files to experiment with level design and drag and drop

---
+++ testing files/index.js
@@ -0,0 +1,86 @@
+// Example of how a level backend might look
+
+let coordinates = [0,0];
+
+console.log('TRY TO GET TO [1, 1]!');
+
+const blastoff = new Promise((planA, planB) => {
+  console.log('blasting off');
+  ascent();
+  planA();
+});
+
+blastoff.then((input) => {
+  console.log('made it to', coordinates);
+  console.log('trying to go right');
+  goRight();
+  console.log('landed at', coordinates);
+}).catch((input) => {
+  console.log('rerouted to', coordinates);
+  console.log('trying to recover');
+  console.log('recovered from:', input);
+  goUp();
+  console.log('landed at', coordinates);
+}).then((input) => {
+  console.log('DID YOU MAKE IT?');
+})
+
+function ascent(input) {
+  console.log('going up from', coordinates);
+  const result = Math.random();
+  if (result > 0.5) {
+    console.log('oh no');
+    coordinates[0] += 1;
+    throw 'blast off fail';
+  } else {
+    coordinates[1] += 1;
+  }
+}
+
+function goUp(input) {
+  console.log('going up');
+  const result = Math.random();
+  if (result > 0) {
+    coordinates[1] += 1;
+    return coordinates;
+  } else {
+    console.log('oh no');
+    throw 'forward fail';
+  }
+}
+
+function goDown(input) {
+  console.log('going down');
+  const result = Math.random();
+  if (result > 0) {
+    coordinates[1] -= 1;
+    return coordinates;
+  } else {
+    console.log('oh no');
+    throw 'back fail';
+  }
+}
+
+function goRight(input) {
+  console.log('going right');
+  const result = Math.random();
+  if (result > 0) {
+    coordinates[0] += 1;
+    return coordinates;
+  } else {
+    console.log('oh no');
+    throw 'right fail';
+  }
+}
+
+function goLeft(input) {
+  console.log('going left');
+  const result = Math.random();
+  if (result > 0.5) {
+    coordinates[0] -= 1;
+    return coordinates;
+  } else {
+    console.log('oh no');
+    throw 'left fail';
+  }
+}
